58d05afd640fc3d773e8fd21cf1bf512c43951fe
Jason Ovenden <118778844+Ovenator27@users.noreply.github.com>
2024-08-28 11:19:18 +0100

    Refactor to use list comprehension

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ pen.penup()
 pen.hideturtle()
 
 state_data = pandas.read_csv("50_states.csv")
+all_states = state_data.state.to_list()
 correct_guesses = []
 
 game_over = False
@@ -19,7 +20,7 @@ while not game_over and len(correct_guesses) < 50:
     answer_state = screen.textinput(title=f"{len(correct_guesses)}/50 States Correct", prompt="Name another state")
     if answer_state == None:
         game_over = True
-    elif answer_state.title() in state_data.state.values and answer_state not in correct_guesses:
+    elif answer_state.title() in all_states and answer_state not in correct_guesses:
         state = state_data[state_data.state == answer_state.title()]
         x_cor = int(state.x.iloc[0])
         y_cor = int(state.y.iloc[0])
@@ -28,12 +29,8 @@ while not game_over and len(correct_guesses) < 50:
         correct_guesses.append(state.state.item())
     
 remaining_states = {
-    "State": []
+    "State": [state for state in all_states if state not in correct_guesses]
     }
-
-for state in state_data.state.values:
-    if state not in correct_guesses:
-        remaining_states["State"].append(state)
 
 df = pandas.DataFrame(remaining_states)
 df.to_csv("remaining_states.csv")
